a08f0005e11c0aaa4745204a5186a69150033783
Raditya Praba M <raditya.mayantara@bfi.co.id>
2024-07-04 11:50:30 +0700

    chore: update config and some package

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig, loadEnv } from 'vite'
-import tsconfigPaths from 'vite-tsconfig-paths'
+import react from '@vitejs/plugin-react';
+import { ConfigEnv } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
-export default ({ mode }: any) => {
-  console.log('mode', mode)
-  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
+export default ({ mode }: ConfigEnv) => {
+  console.log('mode', mode);
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
 
-  const serverPort: number = parseInt(`${process.env.VITE_PORT}`, 10) || 3000
+  const serverPort: number = parseInt(`${process.env.VITE_PORT}`, 10) || 3000;
 
   return defineConfig({
     build: {
@@ -20,5 +21,5 @@ export default ({ mode }: any) => {
     server: {
       port: serverPort,
     },
-  })
-}
+  });
+};
